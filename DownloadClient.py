8590d838ae9d0650b6135fa5fb32a10ee948684d
Bastian Buman <bastian.buman@geo.uzh.ch>
2020-08-21 13:25:58 +0200

    Update DownloadClient.py

--- DownloadClient.py
+++ DownloadClient.py
@@ -114,83 +114,88 @@ class MyApp(tkinter.Frame):
         self.connect["state"] = tkinter.ACTIVE
 
     def createConnectionDialogWidgets(self):
+        self.connection_frame = tkinter.Frame(self.frame)
+        self.connection_frame.pack()
+        self.label_frames = tkinter.Frame(self.connection_frame)
+        self.label_frames.pack(side=tkinter.LEFT)
+
+        self.entry_frames = tkinter.Frame(self.connection_frame)
+        self.entry_frames.pack(side=tkinter.RIGHT)
+
+
         ## Server
-        self.server = tkinter.Label(self.frame)
-        self.server.pack(fill='both', expand=True, side="right", padx=10, pady=10)
+        self.server = tkinter.Label(self.label_frames)
+        self.server.pack(padx=5, pady=5)
         self.server["text"] = "Web Application Server"
-        self.entryServer = tkinter.Entry(self.frame)
-        self.entryServer.pack(fill='both', expand=True, side="right", padx=10, pady=10)
+        self.entryServer = tkinter.Entry(self.entry_frames)
+        self.entryServer.pack(padx=5, pady=5)
         self.serverName = tkinter.StringVar()
         self.serverName.set("")
         self.entryServer["textvariable"] = self.serverName
 
         ## Port
-        self.port = tkinter.Label(self.frame)
-        self.port.pack(fill='both', expand=True, side="right", padx=10, pady=10)
+        self.port = tkinter.Label(self.label_frames)
+        self.port.pack(padx=5, pady=5)
         self.port["text"] = "Port"
-        self.entryPort = tkinter.Entry(self.frame)
-        self.entryPort.pack(fill='both', expand=True, side="right", padx=10, pady=10)
+        self.entryPort = tkinter.Entry(self.entry_frames)
+        self.entryPort.pack(padx=5, pady=5)
         self.portName = tkinter.StringVar()
         self.portName.set("")
-        self.entryServer["textvariable"] = self.portName
-
+        self.entryPort["textvariable"] = self.portName
 
         ## Application Path
-        self.path = tkinter.Label(self.frame)
-        self.path.pack(fill='both', expand=True, side="right", padx=10, pady=10)
+        self.path = tkinter.Label(self.label_frames)
+        self.path.pack(padx=5, pady=5)
         self.path["text"] = "Application Path"
-        self.entryPath = tkinter.Entry(self.frame)
-        self.entryPath.pack(fill='both', expand=True, side="right", padx=10, pady=10)
+        self.entryPath = tkinter.Entry(self.entry_frames)
+        self.entryPath.pack(padx=5, pady=5)
         self.pathName = tkinter.StringVar()
         self.pathName.set("")
-        self.entryServer["textvariable"] = self.pathName
-
+        self.entryPath["textvariable"] = self.pathName
 
         ## Data Source Name
-        self.datasource = tkinter.Label(self.frame)
-        self.datasource.pack(fill='both', expand=True, side="right", padx=10, pady=10)
+        self.datasource = tkinter.Label(self.label_frames)
+        self.datasource.pack(padx=5, pady=5)
         self.datasource["text"] = "Data Source Name"
-        self.entryDatasource = tkinter.Entry(self.frame)
-        self.entryDatasource.pack(fill='both', expand=True, side="right", padx=10, pady=10)
+        self.entryDatasource = tkinter.Entry(self.entry_frames)
+        self.entryDatasource.pack(padx=5, pady=5)
         self.datasourceName = tkinter.StringVar()
         self.datasourceName.set("")
-        self.entryServer["textvariable"] = self.datasourceName
-
+        self.entryDatasource["textvariable"] = self.datasourceName
 
         ## Username
-        self.username = tkinter.Label(self.frame)
-        self.username.pack(fill='both', expand=True, side="right", padx=10, pady=10)
+        self.username = tkinter.Label(self.label_frames)
+        self.username.pack(padx=5, pady=5)
         self.username["text"] = "Username"
-        self.entryUsername = tkinter.Entry(self.frame)
-        self.entryUsername.pack(fill='both', expand=True, side="right", padx=10, pady=10)
+        self.entryUsername = tkinter.Entry(self.entry_frames)
+        self.entryUsername.pack(padx=5, pady=5)
         self.usernameName = tkinter.StringVar()
         self.usernameName.set("")
-        self.entryServer["textvariable"] = self.usernameName
-
+        self.entryUsername["textvariable"] = self.usernameName
 
         ## Password
-        self.password = tkinter.Label(self.frame)
-        self.password.pack(fill='both', expand=True, side="right", padx=10, pady=10)
+        self.password = tkinter.Label(self.label_frames)
+        self.password.pack(padx=5, pady=5)
         self.password["text"] = "Password"
-        self.entryPassword = tkinter.Entry(self.frame)
-        self.entryPassword.pack(fill='both', expand=True, side="right", padx=10, pady=10)
+        self.entryPassword = tkinter.Entry(self.entry_frames)
+        self.entryPassword.pack(padx=5, pady=5)
         self.passwordName = tkinter.StringVar()
         self.passwordName.set("")
-        self.entryServer["textvariable"] = self.passwordName
-
+        self.entryPassword["textvariable"] = self.passwordName
 
         ## Connect
-        self.connect = tkinter.Button(self.frame)
-        self.connect.pack(fill='both', expand=True, side="right", padx=10, pady=10)
+        self.connect = tkinter.Button(self.label_frames)
+        self.connect.pack(padx=5, pady=5)
         self.connect["text"] = "Connect"
         self.connect["state"] = tkinter.ACTIVE
         self.connect["command"] = self.connectAndDestroy
 
         ## Cancel
-        self.cancel = tkinter.Button(self.frame)
-        self.cancel.pack(fill='both', expand=True, side="right", padx=10, pady=10)
+        self.cancel = tkinter.Button(self.entry_frames)
+        self.cancel.pack(padx=5, pady=5)
         self.cancel["text"] = "Cancel"
         self.cancel["command"] = self.frame.destroy
+
 
     def connectAndDestroy(self):
         self.frame.destroy()
@@ -201,15 +206,15 @@ class MyApp(tkinter.Frame):
         self.SPECCHIO = jp.JPackage('ch').specchio.client
         # 1.3 Create a client factory instance and get a list of all connection details:
         self.cf = self.SPECCHIO.SPECCHIOClientFactory.getInstance()
-        # self.db_descriptor_list = self.cf.getAllServerDescriptors()
+        #self.db_descriptor_list = self.cf.getAllServerDescriptors()
         # 1.4 Connect to specchio:
-        self.db_descriptor = self.SPECCHIO.SPECCHIODatabaseDescriptor(self.serverName.get(),
-                                                             self.portName.get(),
-                                                             self.pathName.get(),
-                                                             self.datasourceName.get(),
-                                                             self.usernameName.get(),
-                                                             self.passwordName.get())
-        # self.specchio_client = self.cf.createClient(self.db_descriptor_list.get(1))  # zero indexed
+        self.db_descriptor = self.SPECCHIO.SPECCHIODatabaseDescriptor(jp.JString(self.serverName.get()),
+                                                             jp.JInt(int(self.portName.get())),
+                                                             jp.JString(self.pathName.get()),
+                                                             jp.JString(self.datasourceName.get()),
+                                                             jp.JString(self.usernameName.get()),
+                                                             jp.JString(self.passwordName.get()))
+        #self.specchio_client = self.cf.createClient(self.db_descriptor_list.get(1))  # zero indexed
         self.specchio_client = self.cf.createClient(self.db_descriptor)  # zero indexed
         self.buildTree()
 
